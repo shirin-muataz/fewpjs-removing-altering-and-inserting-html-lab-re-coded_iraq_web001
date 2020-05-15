c75379242777c18aa9c057197367670d0c2e5d64
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-15 10:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Write your code here!
 document.querySelector('main#main');
 main.remove();
-let =document.greateElement;
-let h1=document.createElement('h1');
-document.body.appendChild(h1);
-h1.nodeName='h1';
-h1.id='victory';
+let newHeader=document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
