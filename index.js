94f6831149065c87104de48dac245decbfc6be83
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-15 10:21:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main#main');
+main.remove();
+let newHeader=document.greateElement;
+let h1=document.createElement('h1');
+document.body.appendChild(h1);
+h1.nodeName='h1';
+h1.id='victory';
+newHeader=document.querySelector('h1#victory');
