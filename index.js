ecc711a8434c70bf17c4c6752c9eed8aeab12db3
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-15 10:23:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main');
+main.remove();
+let newHeader=document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+newHeader.innerHTML='Shirin is the champion';
